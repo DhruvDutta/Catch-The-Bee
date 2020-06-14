7d7259fa81180d06c4cad22aeb874b3d1aab6514
DhruvDutta <54740423+DhruvDutta@users.noreply.github.com>
2020-06-14 15:58:32 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,7 +5,9 @@ let interval;
 let level;
 let play;
 let pre_x=window.innerWidth/2;
-
+let bee;
+let score =0;
+let scoreText ='';
 if(localStorage.getItem('highscore')==null){
     localStorage.setItem('highscore',0)
 }else{
@@ -37,15 +39,14 @@ function create(){
     this.back = this.add.sprite(W/2,H/2,'back').setScale(.6)
     this.back.setInteractive();
     head = this.add.text(20,10,'Catch The Bee',{ font: `30px Arial`, fill: '#fff' })
-    this.score = 0;
-    this.scoreText = '';
+
     this.highscore = '';
-    this.bee = this.add.sprite(W/2,H/2-50,'bee').setScale(W*H/3000000);
-    this.bee.setInteractive();
+    bee = this.add.sprite(W/2,H/2-50,'bee').setScale(W*H/3000000);
+    bee.setInteractive();
     play = this.add.text(W/2-60,H/2,'Play',{font:'70px Arial',fill:'#fff',align:'center'})
     play.alpha=.2;
     this.tweens.add({
-        targets: [this.bee],
+        targets: [bee],
         y: W/2+10,
         duration: 1500,
         ease: 'Sine.easeInOut',
@@ -70,8 +71,8 @@ function update(){
 function run(){
     this.tweens.killTweensOf(play)
     
-    interval = setInterval(appear,t,this.bee);
-    this.bee.on('pointerdown',addscore,this);
+    interval = setInterval(appear,t,bee);
+    bee.on('pointerdown',addscore,this);
     this.back.on('pointerdown',subscore,this);
 
     this.input.off('pointerdown');
@@ -79,38 +80,38 @@ function run(){
     play.setText("")
     let style = { font: `20px Arial`, fill: '#fff' };
 
-    this.scoreText = this.add.text(10, 30, 'score: ' + this.score, style);
+    scoreText = this.add.text(10, 30, 'score: ' + score, style);
     this.highscore = this.add.text(10, 10, 'HighScore: ' + localStorage.getItem('highscore'), style);
 
-    level = setInterval(levelup,12000,this.bee);
+    level = setInterval(levelup,12000,bee);
 
 }
 function appear(q){
     q.x = Phaser.Math.Between(50,game.config.width-30);
     q.y = Phaser.Math.Between(50,game.config.width-30);
-    if(q.x>pre_x){
-        q.setFlip(true,false);
-        pre_x=q.x;
+    if(bee.x>pre_x){
+        bee.setFlip(true,false);
+        pre_x=bee.x;
     }else{
-        q.setFlip(false,false);
-        pre_x=q.x;
+        bee.setFlip(false,false);
+        pre_x=bee.x;
     }
     
 }
 
 function addscore(){
-    this.score+=point;
-    this.scoreText.setText('score: ' + this.score);
-    if(localStorage.getItem('highscore')<this.score){
-        localStorage.setItem('highscore',this.score)
-        this.highscore.setText('HighScore:'+this.score)
+    score+=point;
+    scoreText.setText('score: ' + score);
+    if(localStorage.getItem('highscore')<score){
+        localStorage.setItem('highscore',score)
+        this.highscore.setText('HighScore:'+score)
     }
 
 }
 function subscore(){
-    this.score-=point/2;
-    this.scoreText.setText('score: ' + this.score);
-    if(this.score<0){
+    score-=point/2;
+    scoreText.setText('score: ' + score);
+    if(score<0){
         document.write('<center><h2 style="margin-top:50vh;">Game Over</h2><hr><a href="https://dhruvdutta.github.io/mywebsite/">View More</a><hr></center>')
         throw new Error('Game Over');
     };
